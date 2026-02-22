d4a36450b9f8359b29f292c533a645be61ca5efb
MatoxEdriel <ga99_buggi@hotmail.com>
2026-02-21 21:15:49 -0500

    Practica

--- Auth.Api/obj/Debug/net9.0/Auth.Api.AssemblyInfo.cs
+++ Auth.Api/obj/Debug/net9.0/Auth.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Auth.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce554e8b38018485de4404e720e56dc5e64e953d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b3c05f04b61948c192616d3c8dfa7866de2de13")]
 [assembly: System.Reflection.AssemblyProductAttribute("Auth.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Auth.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
